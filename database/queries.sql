4c6f4779f40d33df8a49f2c9b8a777cf97a43a3b
Mika <shiffmanm@carleton.edu>
2023-04-27 20:49:56 -0500

    sql

---
+++ database/queries.sql
@@ -0,0 +1,5 @@
+SELECT county, states FROM counties ORDER BY population2021 LIMIT 10;
+
+SELECT county, states FROM counties ORDER BY realGDP2021 LIMIT 10;
+
+SELECT county, states FROM counties ORDER BY realGDP2021/population2021 LIMIT 10;
