3b6c5c4435f8d4a7861aecbc10739f1a6d661f47
Mika <shiffmanm@carleton.edu>
2023-05-01 16:44:52 -0500

    sql

--- database/queries.sql
+++ database/queries.sql
@@ -1,5 +1,7 @@
-SELECT county, states, population2021 FROM counties ORDER BY population2021 LIMIT 10;
+SELECT county, states, population2021 FROM counties WHERE population2021 IS NOT NULL ORDER BY population2021 DESC LIMIT 10;
 
-SELECT county, states, realGDP2021 FROM counties ORDER BY realGDP2021 LIMIT 10;
+SELECT county, states, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL ORDER BY realgdp2021 DESC LIMIT 10;
 
-SELECT county, states FROM counties ORDER BY realGDP2021/population2021 LIMIT 10;
+SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
+
+SELECT county, states, AVG(medianAQI) FROM counties WHERE states = Alabama; 
