e85d48e29e417ee855a178b3e38ddc5a1b6f46e7
Mika <shiffmanm@carleton.edu>
2023-05-01 17:04:05 -0500

    sql

--- database/sampleQueries.sql
+++ database/sampleQueries.sql
@@ -5,4 +5,4 @@ SELECT county, states, population2021 FROM counties WHERE population2021 IS NOT 
 SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
 
 /* Lists the average median AQI of a state */
-SELECT states, AVG(medianAQI) FROM counties GROUP BY states;
+SELECT states, AVG(medianAQI) FROM counties WHERE medianAQI IS NOT NULL GROUP BY states;
