c959eddc8de0a51b31ed14dea9f599717a9ba18a
Mika <shiffmanm@carleton.edu>
2023-05-01 17:00:54 -0500

    sql

--- database/sampleQueries.sql
+++ database/sampleQueries.sql
@@ -1,5 +1,8 @@
+/* Lists counties by population, in descending order (displays 10) */
 SELECT county, states, population2021 FROM counties WHERE population2021 IS NOT NULL ORDER BY population2021 DESC LIMIT 10;
 
+/* Lists counties by realGDP per capita in descending order (displays 10) */
 SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
 
-SELECT AVG(medianAQI) FROM counties WHERE states = 'Alabama'; 
+/* Lists the average median AQI of a state */
+SELECT AVG(medianAQI) FROM counties GROUP BY states;
