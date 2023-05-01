71d58d41eea4e62010ea0f40a9a470f15b70e29f
Mika <shiffmanm@carleton.edu>
2023-05-01 16:49:01 -0500

    sql

--- database/queries.sql
+++ database/queries.sql
@@ -4,4 +4,4 @@ SELECT county, states, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL O
 
 SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
 
-SELECT county, states, AVG(medianAQI) FROM counties WHERE states = Alabama; 
+SELECT AVG(medianAQI) FROM counties WHERE states = 'Alabama'; 
