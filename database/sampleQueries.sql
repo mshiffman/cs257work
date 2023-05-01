bed96b9e863ea8eac694f9e21d0bd93ce0d22f3a
Mika <shiffmanm@carleton.edu>
2023-05-01 16:57:05 -0500

    sql

---
+++ database/sampleQueries.sql
@@ -0,0 +1,5 @@
+SELECT county, states, population2021 FROM counties WHERE population2021 IS NOT NULL ORDER BY population2021 DESC LIMIT 10;
+
+SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
+
+SELECT AVG(medianAQI) FROM counties WHERE states = 'Alabama'; 
