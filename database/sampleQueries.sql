c9e58219041df0533847c9ec0be98c7447a0f691
Mika <shiffmanm@carleton.edu>
2023-05-01 17:11:04 -0500

    sql

--- database/sampleQueries.sql
+++ database/sampleQueries.sql
@@ -4,5 +4,9 @@ SELECT county, states, population2021 FROM counties WHERE population2021 IS NOT 
 /* Lists counties by realGDP per capita in descending order (displays 10) */
 SELECT county, states, population2021, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND population2021 IS NOT NULL ORDER BY realgdp2021/population2021 DESC LIMIT 10;
 
+
+SELECT county, states, ninetyPercentileAQI, realGDP2021 FROM counties WHERE realGDP2021 IS NOT NULL AND ninetyPercentileAQI IS NOT NULL ORDER BY realGDP2021
+
 /* Lists the average median AQI of a state */
+/* There is no data for Alaska because the EPA has no air quality monitors there */
 SELECT states, AVG(medianAQI) FROM counties WHERE medianAQI IS NOT NULL GROUP BY states;
